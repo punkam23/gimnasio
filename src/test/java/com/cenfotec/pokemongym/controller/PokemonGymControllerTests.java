d9427c4dd83f3872a66246fcc27313199974b1b6
Yehoshua M.V <yosh.mataval@gmail.com>
2023-12-09 20:29:28 -0600

    arreglando test

--- src/test/java/com/cenfotec/pokemongym/controller/PokemonGymControllerTests.java
+++ src/test/java/com/cenfotec/pokemongym/controller/PokemonGymControllerTests.java
@@ -14,7 +14,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
-@SpringBootTest
+//@SpringBootTest
 class PokemonGymControllerTests {
 
     @InjectMocks
